fe06149318b90682dcba438db9cc8681443c3b2c
Heinz <heinz.samuelsson@gmail.com>
2017-03-30 23:11:52 +0200

    global var added

--- fsm/fsm_test1.py
+++ fsm/fsm_test1.py
@@ -1,3 +1,5 @@
+var = 0
+
 class bcolors:
     HEADER = '\033[95m'
     BLUE = '\033[94m'
@@ -19,48 +21,60 @@ class FSM():
 
     def run(self):
         if (self.running == 0):
-	    self.nx = self.startState(self.cargo)
+	    self.nx, self.cargo = self.startState(self.cargo)
 	    self.running = 1
 	    print 'cargo:', self.cargo
+
 	else:
 	    print '    running'
-	    self.nx = self.nx(self.cargo)
+	    self.nx, self.cargo = self.nx(self.cargo)
 
 
 
-# FSM2
+# FSM1
 def FSM1_state1(cargo):
+    global var
+    var += 1
+    cargo = var
     print bcolors.GREEN + '  FSM1_state1' + bcolors.ENDC
+    print '11 cargo', cargo
     nextState = FSM1_state2
-    var =+ 1
-    return nextState
+    return nextState, cargo
 
 def FSM1_state2(cargo):
+    global var
+    var += 1
+    cargo = var
     print bcolors.GREEN + '  FSM1_state2' + bcolors.ENDC
+    print '12 cargo', cargo
     nextState = FSM1_state3
-    return nextState
+    return nextState, cargo
 
 def FSM1_state3(cargo):
+    global var
+    var += 1
+    cargo = var
     print bcolors.GREEN + '  FSM1_state3' + bcolors.ENDC
+    print '13 cargo', cargo
     nextState = FSM1_state1
-    return nextState
+    return nextState, cargo
 
 
 # FSM2
 def FSM2_state1(cargo):
     print bcolors.RED + '  FSM2_state1' + bcolors.ENDC
     nextState = FSM2_state2
-    return nextState
+    return nextState, cargo
 
 def FSM2_state2(cargo):
     print bcolors.RED + '  FSM2_state2' + bcolors.ENDC
     nextState = FSM2_state3
-    return nextState
+    return nextState, cargo
 
 def FSM2_state3(cargo):
     print bcolors.RED + '  FSM2_state3' + bcolors.ENDC
     nextState = FSM2_state1
-    return nextState
+    return nextState, cargo
 
 
 fsm1 = FSM(FSM1_state1, 1)
