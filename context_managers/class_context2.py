0ae13a4bbc3e605cc3f1ec80cd6e9ab77284552d
heinz.samuelsson <heinz.samuelsson@unjo.com>
2018-02-21 17:01:37 +0100

    Code cleanup _context2

--- context_managers/class_context2.py
+++ context_managers/class_context2.py
@@ -2,7 +2,7 @@
 # Date        : Fri Mar 25 10:38:15 CET 2016
 # File        : class_context2.py
 # Reference   : http://arnavk.com/posts/python-context-managers/
-# Description : Not working example.
+# Description : Not working example. Need a server to connect to ...
 #
 # Python ver  : Python 2.7
 
@@ -10,25 +10,34 @@ import socket
 
 class SomeProtocol:
 
-     def __init__(self, host, port):
-          self.host, self.port = host, port
+    def __init__(self, host, port):
+        print '__init__ host:{}, port:{}'.format(host, port)
+        self.host = host
+        self.port = port
 
-     def __enter__(self):
-          self._client = socket.socket()
-          self._client.connect((self.host, self.port))
-          return self
+    def __enter__(self):
+        print '__enter__' 
+        self._client = socket.socket()
+        self._client.connect((self.host, self.port))
+        return self
 
-     def __exit__(self, exception, value, traceback):
-          self._client.close()
+    def __exit__(self, exception, value, traceback):
+        print '__exit__'
+        self._client.close()
 
-     def send(self, payload): ...
+    def send(self, payload):
+        pass
 
-     def receive(self): ...
+    def receive(self):
+        pass
 
 
 host = '127.0.0.1'
+#host = socket.gethostname()
 port = 8888
+
 
 with SomeProtocol(host, port) as protocol:
      protocol.send(['get', signal])
      result = protocol.receive()
+
