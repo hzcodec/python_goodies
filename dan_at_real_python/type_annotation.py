82f7acb3f4c46317299627d7c9c802533acfaced
heinz.samuelsson <heinz.samuelsson@unjo.com>
2018-08-22 09:19:30 +0200

    dan_at_real updated

---
+++ dan_at_real_python/type_annotation.py
@@ -0,0 +1,8 @@
+# Python 3.5+ supports 'type annotations' that can be
+# used with tools like Mypy to write statically typed Python:
+
+def my_add(a: int, b: int) -> int:
+    return a + b
+
+rv = my_add(12, 22)
+print (rv)
