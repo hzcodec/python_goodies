2ba5a1b456137651f79cf7b1127fcca28b3daee6
heinz.samuelsson <heinz.samuelsson@unjo.com>
2018-08-28 16:01:27 +0200

    Updated dan...

---
+++ dan_at_real_python/peek_behind_the_bytecode.py
@@ -0,0 +1,17 @@
+# You can use Python's built-in "dis"
+# module to disassemble functions and
+# inspect their CPython VM bytecode:
+
+def greet(name):
+	return 'Hello, ' + name + '!'
+
+print greet('Jennie')
+
+import dis
+dis.dis(greet)
+#2   0 LOAD_CONST     1 ('Hello, ')
+#    2 LOAD_FAST      0 (name)
+#    4 BINARY_ADD
+#    6 LOAD_CONST     2 ('!')
+#    8 BINARY_ADD
+#   10 RETURN_VALUE
