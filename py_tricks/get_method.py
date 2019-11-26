cf1e581ce39a1e3d48c4ec101f406d3de5212501
Heinz Samuelsson <heinz.samuelsson@gmail.com>
2019-11-26 19:46:12 +0100

    Mupp

--- py_tricks/get_method.py
+++ py_tricks/get_method.py
@@ -6,15 +6,32 @@
 #If it does not exist then the value of the default argument is returned instead.
 
 
-name_for_userid = {
-    382: "Alice",
-    590: "Bob",
+#name_for_userid = {
+#    382: "Alice",
+#    590: "Bob",
+#    951: "Dilbert",
+#}
+
+def f1():
+    print('Yes')
+ 
+
+getit = {
+    'hello': "there",
+    'oh': "shit",
     951: "Dilbert",
+    'mark': f1
 }
 
-def greeting(userid):
-    return "Hi %s!" % name_for_userid.get(userid, "there")
+#def greeting(userid):
+#    return "Hi %s!" % name_for_userid.get(userid, "there")
 
-print greeting(382)
+def pef(userid):
+    return "wtf %s!" % getit.get(userid, "oh no")
 
-print greeting(333)
+#print greeting(382)
+#print greeting(333)
+
+print pef('hello')
+print pef('jennie')
+x = pef('mark')
